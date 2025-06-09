b96c8a49ee6ff66a2c5676fd2ea06b7f4f99dc4d
Relrin <relrin78@gmail.com>
2025-06-09 02:16:44 +0200

    Removed unused type

--- src/storage.rs
+++ src/storage.rs
@@ -3,15 +3,6 @@ use std::sync::Arc;
 use serenity::model::id::ApplicationId;
 use serenity::prelude::TypeMapKey;
 
-use crate::commands::giveaway::manager::GiveawayManager;
-
-// TODO: Remove this???
-pub struct GiveawayStorage;
-
-impl TypeMapKey for GiveawayStorage {
-    type Value = Arc<GiveawayManager>;
-}
-
 pub struct BotIdStorage;
 
 impl TypeMapKey for BotIdStorage {
